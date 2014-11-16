044d58dcca3c37dea096a1283f26ddf56898b990
Kurtis Brown <kurtis@kurtisebear.com>
2014-11-16 15:51:35 +0000

    fixed typo

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 # Set Gmail Credentials
 os.environ['GMAIL_USERNAME'] = "username@gmail.com"
-os.environ['GMAIL PASSWORD'] = "password"
+os.environ['GMAIL_PASSWORD'] = "password"
 
 #set email address to send to
 os.environ['EMAIL'] = "testemail@gmail.com"
